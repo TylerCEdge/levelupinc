4ceef12b288459f45cfd009bb535de9d146c4491
TylerCEdge <tylercedge@gmail.com>
2020-07-22 16:57:03 -0400

    added name with useState to campure game name input for later use by igdb api

--- frontend/src/shared/components/Home.js
+++ frontend/src/shared/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CenteredContainer from './UIElements/CenteredContainer';
 import Overlay from './UIElements/Overlay';
@@ -6,15 +6,27 @@ import Overlay from './UIElements/Overlay';
 import Button from './UIElements/Button';
 import Input from './UIElements/Input';
 
-export default function Home() {
+export default function Home(props) {
+  const [name, setName] = useState('');
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(`Searching for:... ${name}.`);
+  }
   return (
     <React.Fragment>
       <Overlay>
         <CenteredContainer>
-          <Input placeholder='Search for a game...' />
-          <Button className='btn' type='submit'>
-            Search
-          </Button>
+          <form onSubmit={submitHandler}>
+            <input
+              type='text'
+              placeholder='Search for a game...'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input type='submit' className='btn' value='Submit' />
+          </form>
         </CenteredContainer>
       </Overlay>
     </React.Fragment>
